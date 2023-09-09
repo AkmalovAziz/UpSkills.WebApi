d86f6f37b45da098a91ca40bb6d1df123f8202f9
AkmalovAziz <akmalov844@gmail.com>
2023-09-09 19:39:44 +0500

    Create Helpers

---
+++ src/UpSkills.Persistance/Helpers/TimeHelpers.cs
@@ -0,0 +1,14 @@
+﻿using UpSkills.Domain.Constans;
+
+namespace UpSkills.Persistance.Helpers;
+
+public class TimeHelpers
+{
+    public static DateTime GetDateTime()
+    {
+        var datetime = DateTime.UtcNow;
+        datetime.AddHours(TimeConstans.UTC);
+
+        return datetime;
+    }
+}
